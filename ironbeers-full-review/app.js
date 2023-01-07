b2d3ad467326b494eb491292eae30176072049e1
borbersaur <plasmabeach@gmail.com>
2023-01-07 10:45:39 -0500

    ironbeers full review

---
+++ ironbeers-full-review/app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+
+const hbs = require('hbs');
+const path = require('path');
+const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+
+const app = express();
+const punkAPI = new PunkAPIWrapper();
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, "/views/partials"));
+// ...
+
+// Add the route handlers here:
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers().then((theResult)=>{
+    console.log(theResult);
+    res.render('beers', {theBeers: theResult});
+  })
+  .catch((err)=>{
+    console.log(err);
+  }) 
+});
+
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom().then((theResult)=>{
+    console.log(theResult);
+    res.render('random', {theRandomBeer: theResult[0]});
+  })
+  .catch((err)=>{
+    console.log(err);
+  }) 
+});
+
+
+
+
+
+app.listen(3000, () => console.log('🏃‍ on port 3000'));
